15ac989573fd374ce36714bcc3f44624a859e1c0
Jayant125 <jayantkumar62061@gmail.com>
2025-07-13 14:31:19 +0530

    fix: updated vercel config + backend export

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss()
+//   ],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
+});
